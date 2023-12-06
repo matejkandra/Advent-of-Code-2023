8cd1f33403ca85bec3c4d457a150d5dd251f2d18
Matej Kandra <matej.kandra2006@gmail.com>
2023-12-06 01:22:08 +0100

    more revision

--- Day 1/Trebuchet.cpp
+++ Day 1/Trebuchet.cpp
@@ -1,142 +1,141 @@
 #include <fstream>
-#include <stdio.h>
 #include <iostream>
 
 std::vector<std::string> extractFile(char* name){
-	std::vector<std::string> stg;
-	std::fstream stream(name, std::ios_base::in);
-	
-	if (stream.is_open()) {
+    std::vector<std::string> stg;
+    std::fstream stream(name, std::ios_base::in);
 
-		for(int i = 0; i < 1000; i++){
-			std::string s;
+    if (stream.is_open()) {
 
-			bool stayIn = true;
+        for(int i = 0; i < 1000; i++){
+            std::string s;
 
-			while (stayIn){
-				char c;
+            bool stayIn = true;
 
-				stream.get(c);
+            while (stayIn){
+                char c;
 
-				if(c == '\n'){
-					stayIn = false;
-				}else{
-					s.push_back(c);
-				}
-			}
+                stream.get(c);
 
-			stg.push_back(s);
-		}
+                if(c == '\n'){
+                    stayIn = false;
+                }else{
+                    s.push_back(c);
+                }
+            }
 
-	}
+            stg.push_back(s);
+        }
 
-	stream.close();
-	return stg;
+    }
+
+    stream.close();
+    return stg;
 }
 
 int decodeNumbersAndAddup(std::vector<std::string> v){
-	int num = 0;
+    int num = 0;
 
-	std::unordered_map<std::string, int> checkMap;
-	std::array<std::string, 10> checkList = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"};
+    std::unordered_map<std::string, int> checkMap;
+    std::array<std::string, 10> checkList = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight","nine"};
 
-	for(int i = 0; i < 10; i++){
-		checkMap[checkList.at(i)] = i;
-	}
+    for(int i = 0; i < 10; i++){
+        checkMap[checkList.at(i)] = i;
+    }
 
-	for(std::string x : v){
+    for(std::string x : v){
 
-		char a = 0;
+        char a = 0;
 
-		int frontToBackIndex = 0;
+        int frontToBackIndex = 0;
 
-		for(char y : x){
-			if (isdigit(y)) {
-				a = y;
-				break;
-			}else{
+        for(char y : x){
+            if (isdigit(y)) {
+                a = y;
+                break;
+            }else{
 
-				for(std::string s : checkList){
-					bool really = true;
-					int stringLength = s.length();
+                for(std::string s : checkList){
+                    bool really = true;
+                    int stringLength = s.length();
 
-					for (int i = 0; i < stringLength; i++) {
-						if (frontToBackIndex+i > x.length()) {
-							really = false;
-							break;
-						}
+                    for (int i = 0; i < stringLength; i++) {
+                        if (frontToBackIndex+i > x.length()) {
+                            really = false;
+                            break;
+                        }
 
-						if (x.at(frontToBackIndex+i)!=s.at(i)) {
-							really = false;
-							break;
-						}
-					}
-					if (really) {
-						a = std::to_string(checkMap[s]).front();
-						goto foundFrontToBackSilly;
-					}
-				}
+                        if (x.at(frontToBackIndex+i)!=s.at(i)) {
+                            really = false;
+                            break;
+                        }
+                    }
+                    if (really) {
+                        a = std::to_string(checkMap[s]).front();
+                        goto foundFrontToBackSilly;
+                    }
+                }
 
-			}
-			frontToBackIndex++;
-		}
+            }
+            frontToBackIndex++;
+        }
 
-		foundFrontToBackSilly:
+foundFrontToBackSilly:
 
-		char b = 0;
+        char b = 0;
 
-		int backToFrontIndex = 0;
+        int backToFrontIndex = 0;
 
-		std::reverse(x.begin(), x.end());
+        std::reverse(x.begin(), x.end());
 
-		for(char y : x ){
-			if (isdigit(y)) {
-				b = y;
-				break;
-			}else{
+        for(char y : x ){
+            if (isdigit(y)) {
+                b = y;
+                break;
+            }else{
 
-				for(std::string s : checkList){
-					bool really = true;
-					int stringLength = s.length();
+                for(std::string s : checkList){
+                    bool really = true;
+                    int stringLength = s.length();
 
-					std::string reversedString = s;
-					std::reverse(reversedString.begin(), reversedString.end());
+                    std::string reversedString = s;
+                    std::reverse(reversedString.begin(), reversedString.end());
 
-					for (int i = 0; i < stringLength; i++) {
-						if (backToFrontIndex+i > x.length()) {
-							really = false;
-							break;
-						}
+                    for (int i = 0; i < stringLength; i++) {
+                        if (backToFrontIndex+i > x.length()) {
+                            really = false;
+                            break;
+                        }
 
-						if (x.at(backToFrontIndex+i)!=reversedString.at(i)) {
-							really = false;
-							break;
-						}
-					}
-					if (really) {
-						b = std::to_string(checkMap[s]).front();
-						goto foundBackToFrontSilly;
-					}
-				}
+                        if (x.at(backToFrontIndex+i)!=reversedString.at(i)) {
+                            really = false;
+                            break;
+                        }
+                    }
+                    if (really) {
+                        b = std::to_string(checkMap[s]).front();
+                        goto foundBackToFrontSilly;
+                    }
+                }
 
-			}
-			backToFrontIndex++;
-		}
+            }
+            backToFrontIndex++;
+        }
 
-		foundBackToFrontSilly:
+foundBackToFrontSilly:
 
-		std::string buf;
-		buf.push_back(a);
-		buf.push_back(b);
+        std::string buf;
+        buf.push_back(a);
+        buf.push_back(b);
 
-		num += std::stoi(buf);	
-	}
-	return num;
+        num += std::stoi(buf);	
+    }
+    return num;
 }
 
 int main(){
-	auto xf = extractFile((char*)"input.txt");
-	std::cout << decodeNumbersAndAddup(xf) << '\n';
+    auto xf = extractFile((char*)"input.txt");
+    std::cout << decodeNumbersAndAddup(xf) << '\n';
 
-	return 0;
+    return 0;
 }
